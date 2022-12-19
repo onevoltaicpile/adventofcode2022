03f41d76262866a0d21b2d0de738220e50410bfd
Aaron Alexander <aaron@workplay.dev>
2022-12-19 18:21:42 -0500

    Result for Day 1 in Python

---
+++ 1/maxcalories.py
@@ -0,0 +1,13 @@
+with open('./input.txt', 'r') as file:
+    caloriessum = 0
+    calorieslist = []
+    for line in file:
+        if len(line.strip())>0:
+            caloriessum = caloriessum + int(line.strip())
+        else:
+            calorieslist.append(caloriessum)
+            caloriessum = 0
+    calorieslist.sort(reverse=True)
+    print("The most calories an elf is carrying: " + str(calorieslist[0]))
+    caloriessumtopthree = sum(calorieslist[0:3])
+    print("The total calories carried by the top 3 elves: " + str(caloriessumtopthree))
